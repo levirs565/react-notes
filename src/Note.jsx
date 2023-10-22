50b60d34d93fd2d2bb8a6b290c3cb901868c5a4a
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2023-10-22 12:26:10 +0700

    add main page

---
+++ src/Note.jsx
@@ -0,0 +1,33 @@
+import "./Note.css";
+
+export function Note({ title, body, archived, createdAt }) {
+  return (
+    <li className="note">
+      <h3 className="note--title">{title}</h3>
+      <time className="note--created-date">{createdAt}</time>
+      <p className="note--body">{body}</p>
+      <div className="note--buttons">
+        <button className="button">
+          {archived ? "Kembalikan" : "Arsipkan"}
+        </button>
+        <button className="button button--danger">Hapus</button>
+      </div>
+    </li>
+  );
+}
+
+export function NoteList({ list }) {
+  return (
+    <ul className="note-list">
+      {list.map(({ id, title, body, archived, createdAt }) => (
+        <Note
+          key={id}
+          title={title}
+          body={body}
+          archived={archived}
+          createdAt={createdAt}
+        />
+      ))}
+    </ul>
+  );
+}
---
+++ src/TopBar.jsx
@@ -0,0 +1,9 @@
+import "./TopBar.css";
+
+export function TopBar() {
+  return (
+    <header className="top-bar">
+      <h1 className="top-bar--title">Notes</h1>
+    </header>
+  );
+}

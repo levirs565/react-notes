53211903f5c66aa4d42deb958ec0085af4794f37
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2023-10-22 13:46:58 +0700

    add note

---
+++ src/AddNoteDialog.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import "./AddNoteDialog.css";
+
+export default class AddNoteDialog extends React.Component {
+  constructor() {
+    super();
+
+    this.dialogElement = null;
+    this.setDialogElementRef = (element) => {
+      this.dialogElement = element;
+    };
+    this.onDialogCloseHandler = () => {
+      this.props.onChange(false);
+    };
+
+    this.state = {
+      title: "",
+      body: "",
+    };
+
+    this.onBodyInputChangeHandler = (inputEl) => {
+      this.setState({
+        ...this.state,
+        body: inputEl.target.value,
+      });
+    };
+
+    this.onTitleInputChangeHandler = (inputEl) => {
+      this.setState({
+        ...this.state,
+        title: inputEl.target.value,
+      });
+    };
+
+    this.onAddButtonClickHandler = () => {
+      this.props.onSubmit({
+        title: this.state.title,
+        body: this.state.body,
+      });
+    };
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.open !== prevProps.open) {
+      if (this.props.open) {
+        this.dialogElement.showModal();
+      }
+    }
+  }
+
+  render() {
+    return (
+      <dialog
+        ref={this.setDialogElementRef}
+        className="add-note-dialog"
+        onClose={this.onDialogCloseHandler}
+      >
+        <form method="dialog">
+          <h2 className="add-note-dialog--title">Tambah Catatan</h2>
+          <label className="add-note-dialog--label" htmlFor="titleInput">
+            Judul:{" "}
+          </label>
+          <input
+            className="add-note-dialog--input"
+            id="titleInput"
+            type="text"
+            value={this.state.title}
+            onChange={this.onTitleInputChangeHandler}
+          />
+          <label className="add-note-dialog--label" htmlFor="bodyInput">
+            Konten:
+          </label>
+          <textarea
+            className="add-note-dialog--input"
+            id="bodyInput"
+            value={this.state.body}
+            onChange={this.onBodyInputChangeHandler}
+          ></textarea>
+          <div className="add-note-dialog--buttons">
+            <button className="button">Cancel</button>
+            <button
+              className="button button--primary"
+              onClick={this.onAddButtonClickHandler}
+            >
+              Tambah
+            </button>
+          </div>
+        </form>
+      </dialog>
+    );
+  }
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,65 @@
 import React from "react";
 import { NoteList } from "./Note";
 import { TopBar } from "./TopBar";
+import AddNoteDialog from "./AddNoteDialog";
 
 class App extends React.Component {
   constructor() {
     super();
-    this.notes = [
-      {
-        id: 0,
-        title: "Test",
-        body: "Test Kontent",
-        archived: false,
-        createdAt: new Date().toString(),
-      },
-    ];
+    this.state = {
+      isAddDialogOpen: false,
+      notes: [
+        {
+          id: 0,
+          title: "Test",
+          body: "Test Kontent",
+          archived: false,
+          createdAt: new Date().toString(),
+        },
+      ],
+    };
+
+    this.onAddFabClickHandler = () => {
+      this.setState({
+        ...this.state,
+        isAddDialogOpen: true,
+      });
+    };
+    this.onDialogChangeHandler = (state) => {
+      this.setState({
+        ...this.state,
+        isAddDialogOpen: state,
+      });
+    };
+    this.onDialogSubmitHandler = (data) => {
+      this.setState({
+        ...this.state,
+        notes: [
+          ...this.state.notes,
+          {
+            ...data,
+            id: +new Date(),
+            archived: false,
+            createdAt: new Date().toString(),
+          },
+        ],
+      });
+    };
   }
   render() {
     return (
       <React.Fragment>
         <TopBar />
         <main>
-          <NoteList list={this.notes} />
-          <button className="app-fab">+</button>
+          <NoteList list={this.state.notes} />
+          <button className="app-fab" onClick={this.onAddFabClickHandler}>
+            +
+          </button>
+          <AddNoteDialog
+            open={this.state.isAddDialogOpen}
+            onChange={this.onDialogChangeHandler}
+            onSubmit={this.onDialogSubmitHandler}
+          />
         </main>
       </React.Fragment>
     );

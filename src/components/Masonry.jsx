846f4ba3f0e513b5cce2895f170f784386212d41
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2023-12-20 10:04:36 +0700

    feat: make masonry layout responsive

---
+++ src/components/Masonry.jsx
@@ -0,0 +1,113 @@
+import { useEffect, useRef, useState } from "react";
+import "./Masonry.css";
+
+function isDivider(node) {
+  return node.className === "masonry--divider";
+}
+
+export function Masonry({ as, className, children }) {
+  const As = as;
+  const elementRef = useRef();
+
+  const [maxColumnHeight, setMaxColumnHeight] = useState();
+  const [lineBreakCount, setLineBreakCount] = useState(0);
+
+  const updateSize = () => {
+    const element = elementRef.current;
+
+    const columnCount = parseInt(
+      window.getComputedStyle(element).getPropertyValue("--column-count")
+    );
+    const columnsHeight = new Array(columnCount).fill(0);
+    let skip = false;
+    element.childNodes.forEach((child) => {
+      if (skip) return;
+
+      if (isDivider(child)) {
+        return;
+      }
+
+      const computedStyle = window.getComputedStyle(child);
+      const height = parseInt(computedStyle.height);
+      if (height === 0) {
+        skip = true;
+        return;
+      }
+
+      const totalHeight = height + parseInt(computedStyle.marginBottom);
+
+      const minHeightIndex = columnsHeight.indexOf(Math.min(...columnsHeight));
+      columnsHeight[minHeightIndex] += totalHeight;
+      child.style.order = minHeightIndex;
+    });
+
+    if (!skip) {
+      setMaxColumnHeight(Math.max(...columnsHeight));
+      setLineBreakCount(columnCount - 1);
+    }
+  };
+
+  useEffect(() => {
+    let animationFrame;
+    const requestUpdateSize = () => {
+      animationFrame = requestAnimationFrame(updateSize);
+    };
+    const resizeObserver = new ResizeObserver(() => {
+      requestUpdateSize();
+    });
+    const mutationObserver = new MutationObserver((mutationList) => {
+      mutationList.forEach((mutation) => {
+        mutation.addedNodes.forEach((node) => {
+          if (isDivider(node)) {
+            return;
+          }
+          resizeObserver.observe(node);
+        });
+        mutation.removedNodes.forEach((node) => {
+          if (isDivider(node)) {
+            return;
+          }
+          resizeObserver.unobserve(node);
+          requestUpdateSize();
+        });
+      });
+    });
+
+    elementRef.current.childNodes.forEach((node) =>
+      resizeObserver.observe(node)
+    );
+    mutationObserver.observe(elementRef.current, {
+      childList: true,
+    });
+
+    return () => {
+      if (animationFrame) cancelAnimationFrame(animationFrame);
+      resizeObserver.disconnect();
+      mutationObserver.disconnect();
+    };
+  }, []);
+
+  const dividerList = new Array(lineBreakCount)
+    .fill(0)
+    .map((_, index) => (
+      <span key={index} className="masonry--divider" style={{ order: index }} />
+    ));
+
+  return (
+    <As
+      className={["masonry", className].join(" ")}
+      ref={elementRef}
+      style={{
+        height: maxColumnHeight,
+      }}
+    >
+      {children}
+      {dividerList}
+    </As>
+  );
+}
+
+export function MasonryItem({ as, className, children }) {
+  const As = as;
+  return <As className={["masonry--item", className].join(" ")}>{children}</As>;
+}
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,14 +1,13 @@
-import { AppButton, AppButtonGroup } from "./AppButton";
 import { HighlightText } from "./HighlightText";
 import "./Note.css";
 import { showFormattedDate } from "../utils";
 import { Link, useLocation } from "react-router-dom";
-import { useEffect, useRef, useState } from "react";
+import { Masonry, MasonryItem } from "./Masonry";
 
 export function Note({ id, title, body, createdAt, highlightPattern }) {
   const location = useLocation();
   return (
-    <li className="note">
+    <MasonryItem as="li" className="note">
       <div className="note--header">
         <h3 className="note--title">
           <Link
@@ -28,101 +27,17 @@ export function Note({ id, title, body, createdAt, highlightPattern }) {
       <p className="note--body">
         <HighlightText text={body} pattern={highlightPattern} />
       </p>
-    </li>
+    </MasonryItem>
   );
 }
 
-export function NoteListGrid({ list, highlightPattern }) {
-  const listRef = useRef();
-
-  const columnCount = 4;
-  const [maxColumnHeight, setMaxColumnHeight] = useState();
-  const [lineBreakCount, setLineBreakCount] = useState(0);
-
-  const updateSize = () => {
-    const listElement = listRef.current;
-
-    const columnsHeight = new Array(columnCount).fill(0);
-    let skip = false;
-    listElement.childNodes.forEach((child) => {
-      if (skip) return;
-
-      if (child.className === "masonry-divider") {
-        return;
-      }
-
-      const computedStyle = window.getComputedStyle(child);
-      const height = parseInt(computedStyle.height);
-      if (height === 0) {
-        skip = true;
-        return;
-      }
-
-      const totalHeight = height + parseInt(computedStyle.marginBottom);
-
-      const minHeightIndex = columnsHeight.indexOf(Math.min(...columnsHeight));
-      columnsHeight[minHeightIndex] += totalHeight;
-      child.style.order = minHeightIndex;
-    });
-
-    if (!skip) {
-      setMaxColumnHeight(Math.max(...columnsHeight));
-      setLineBreakCount(columnCount - 1);
-    }
-  };
-
-  useEffect(() => {
-    let animationFrame;
-    const resizeObserver = new ResizeObserver(() => {
-      animationFrame = requestAnimationFrame(updateSize);
-    });
-    const mutationObserver = new MutationObserver((mutationList) => {
-      mutationList.forEach((mutation) => {
-        mutation.addedNodes.forEach((node) => {
-          if (node.className === "masonry-divider") {
-            return;
-          }
-          resizeObserver.observe(node);
-        });
-        mutation.removedNodes.forEach((node) => {
-          if (node.className === "masonry-divider") {
-            return;
-          }
-          resizeObserver.unobserve(node);
-        });
-      });
-    });
-
-    listRef.current.childNodes.forEach((node) => resizeObserver.observe(node));
-    mutationObserver.observe(listRef.current, {
-      childList: true,
-    });
-
-    return () => {
-      if (animationFrame) cancelAnimationFrame(animationFrame);
-      resizeObserver.disconnect();
-      mutationObserver.disconnect();
-    };
-  }, []);
-
-  const dividerList = new Array(lineBreakCount)
-    .fill(0)
-    .map((_, index) => (
-      <span
-        key={index}
-        className="masonry-divider"
-        style={{ order: index + 1 }}
-      ></span>
-    ));
+export function NoteList({ list, highlightPattern, emptyMessage }) {
+  if (list.length === 0) {
+    return <p className="note-empty">{emptyMessage}</p>;
+  }
 
   return (
-    <ul
-      className="note-list"
-      ref={listRef}
-      style={{
-        height: maxColumnHeight,
-      }}
-    >
+    <Masonry as="ul" className="note-list">
       {list.map(({ id, title, body, createdAt }) => (
         <Note
           key={id}
@@ -133,15 +48,6 @@ export function NoteListGrid({ list, highlightPattern }) {
           highlightPattern={highlightPattern}
         />
       ))}
-      {dividerList}
-    </ul>
+    </Masonry>
   );
-}
-
-export function NoteList({ list, highlightPattern, emptyMessage }) {
-  if (list.length === 0) {
-    return <p className="note-empty">{emptyMessage}</p>;
-  }
-
-  return <NoteListGrid list={list} highlightPattern={highlightPattern} />;
 }
